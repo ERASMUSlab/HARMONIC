f9f09ec2d4f8f89b0d5ce40568dd9840c4febe02
ERASMUSlab <madirene@snu.ac.kr>
2026-03-05 12:11:37 +0900

    Update check helpers and add DESeq2/SummarizedExperiment imports

---
+++ COPY.sh
@@ -0,0 +1,5 @@
+cp docs/articles/MANGO-intro.html docs/articles/single-case.html
+cp docs/articles/MANGO-intro.html docs/articles/MANGO-install.html
+cp docs/articles/MANGO-intro.html docs/articles/multi-case.html
+cp docs/articles/MANGO-intro.html docs/articles/vis-single.html
+cp docs/articles/MANGO-intro.html docs/articles/vis-multi.html
